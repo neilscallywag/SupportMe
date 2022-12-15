4ecbf80c1c73ccbb4a135d0850ab22a4e01fe3d8
jsamaze <111722374+jsamaze@users.noreply.github.com>
2022-12-16 00:40:05 +0800

    New DAO classes

--- backend/Model/DBtest_cases.php
+++ backend/Model/DBtest_cases.php
@@ -1,3 +1,4 @@
+<pre>
 <?php
 
 require 'Database.php';
@@ -5,5 +6,26 @@ require 'Database.php';
 $DAO = new UserDAO();
 $result = $DAO->fetch_password("1");
 
+var_dump($result);
+
+$result = $DAO->fetch_name("1");
+
+var_dump($result);
+
+
+$DAO = new CampaignDAO();
+$result = $DAO->fetch_campaign("1");
+
+var_dump($result);
+
+$DAO = new PledgersDAO();
+$result=$DAO->fetch_pledgers("1");
+var_dump($result);
+
+$result=$DAO->add_pledge(4,2);
 var_dump($result)
+
+
+
 ?>
+</pre>
